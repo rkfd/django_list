8e64d80e7c4bf59ab3265256bf74e316a5ee81d7
Satwik Gawand <satwikgawand@gmail.com>
2021-02-07 18:27:27 +0530

    add: urls, views, admin, home

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Search(models.Model):
+    search  = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now=True)
+
+    # return string rather than 'Search object'
+    def __str__(self):
+        return '{}'.format(self.search)
+
+    # change 'Searchs' to 'Searches' (proper plural form)
+    class Meta:
+        verbose_name_plural = 'Searches'
