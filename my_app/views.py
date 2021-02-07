a3593dfd3ab734f44a118fddb1ed496d8afbe6dd
Satwik Gawand <satwikgawand@gmail.com>
2021-02-07 21:19:52 +0530

    add webscrapign and display on frontend

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+import requests
+from requests.compat import quote_plus
+from . import models
+
+BASE_CRAIGSLIST_URL = 'https://mumbai.craigslist.org/search/?query={}'
 
 # Create your views here.
 def home(request):
@@ -7,8 +12,22 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
-    print(search)
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features="html.parser")
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        post_price = post.find(class_='result-price').text
+        final_postings.append((post_title, post_url, post_price))
+
     stuff_for_frontend = {
         'search': search,
+        'final_postings':  final_postings,
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
